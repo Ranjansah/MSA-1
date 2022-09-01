b1c0897617d33682e2246e3b79d6d33db182d306
Ranjan <sahaadi99@gmail.com>
2022-09-01 16:45:47 +0530

    done

---
+++ src/main/java/com/test/electronic/Msa1Application.java
@@ -0,0 +1,13 @@
+package com.test.electronic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Msa1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Msa1Application.class, args);
+	}
+
+}
